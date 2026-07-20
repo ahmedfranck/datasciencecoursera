cc7128b391a43a2ecbc57b0aab1fedd8f6b78084
Ahmed Franck-Aubin Gbadamassi <81176367+ahmedfranck@users.noreply.github.com>
2026-07-20 18:08:52 +0000

    Complete matrix inverse caching assignment

---
+++ cachematrix.R
@@ -0,0 +1,33 @@
+## These functions create a matrix object that stores its inverse and
+## retrieve the cached inverse when it has already been computed.
+
+## Create a matrix object with methods to set/get the matrix and its inverse.
+
+makeCacheMatrix <- function(x = matrix()) {
+        inverse <- NULL
+        set <- function(y) {
+                x <<- y
+                inverse <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(solve) inverse <<- solve
+        getinverse <- function() inverse
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
+}
+
+
+## Return the cached inverse, or compute and cache it when it is unavailable.
+
+cacheSolve <- function(x, ...) {
+        inverse <- x$getinverse()
+        if(!is.null(inverse)) {
+                message("getting cached data")
+                return(inverse)
+        }
+        data <- x$get()
+        inverse <- solve(data, ...)
+        x$setinverse(inverse)
+        inverse
+}
